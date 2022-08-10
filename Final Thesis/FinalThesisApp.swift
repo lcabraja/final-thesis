e53f3f0eb4f77bf1006c763c469fd63d0f32335c
Luka Cabraja <luka@cabraja.eu>
2022-08-10 19:50:38 +0200

    simplified starting point

---
+++ Final Thesis/FinalThesisApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalThesisApp.swift
+//  Final Thesis
+//
+//  Created by doss on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct FinalThesisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
